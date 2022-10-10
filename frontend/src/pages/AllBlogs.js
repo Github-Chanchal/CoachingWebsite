cbd210a3f7d92b79c43df0a94e9d237d6758eb9b
Chanchal <chanchagupta1332@gmail.com>
2022-10-10 20:13:50 +0530

    allBlogs page

--- frontend/src/pages/AllBlogs.js
+++ frontend/src/pages/AllBlogs.js
@@ -1,23 +1,43 @@
 import React, {useEffect,useState} from 'react';
 import { getAllBlogs } from '../api/index.js';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 const AllBlogs= ()=>{
   const [dataa, setdata] = useState();
 
     let data;
-    useEffect(() => {
-        data =  getAllBlogs();
+    const call = () =>{
+      data =  getAllBlogs();
         console.log(data);
         data.then((a)=>{
          console.log(a?.data?.data[1].value)
-     setdata(a?.data?.data[2].value)
+     setdata(a?.data?.data)
         })
-        document.getElementById("index").innerHTML = dataa;
+        // document.getElementById("index").innerHTML = dataa;
+    }
+    useEffect(() => {
+        call()
      
-     },[dataa]);
+     },[]);
 
      return(
-     <div id="index"></div>
+      // <div id="index"></div>
+      (dataa)?dataa.map((item)=>{
+        return(
+          <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={item.thumbnailUrl} />
+        {console.log(item.thumbnailUrl)}
+        <Card.Body>
+          <Card.Title>{item.title}</Card.Title>
+          {/* <Card.Text>
+           {item.value}
+          </Card.Text> */}
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+        )
+      }) :""
 
      );
 
